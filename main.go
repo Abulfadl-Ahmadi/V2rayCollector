59d9cbe7ea87e9245fe3631dd0b5478337d2a950
Abulfadl Ahmadi <abulfadl.ahmadi@gmail.com>
2026-06-21 03:16:26 +0330

    Update main.go

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 
 var (
 	client      = &http.Client{}
-	maxMessages = 50
+	maxMessages = 40
 
 	// uniqueConfigs uses a map of [protocol] -> [raw config] -> [channelName]
 	// This structure guarantees deduplication while remembering the source channel.
